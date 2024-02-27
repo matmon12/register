e077f5ac58b8a4a0737fa394a889dad2636e1a2d
matmon12 <matveym151@gmail.com>
2024-02-27 19:16:00 +0300

    Add config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vite-pack/',
   plugins: [
     vue(),
     ViteImageOptimizer({
@@ -49,6 +50,3 @@ export default defineConfig({
   ],
 });
 
-export default {
-  base: '/rol-shop/'
-}
