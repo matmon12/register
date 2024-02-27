a398f9a62b3a9ca61a08efd509fe80f36861df10
matmon12 <matveym151@gmail.com>
2024-02-27 19:46:12 +0300

    test2

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import Home from './components/Home.vue'
 </script>
 
 <template>
-  <Home msg="kdfhgkldfhgklhdfkgjhysdklfjhgkdjfhgkjdhfsklg" />
+  <Home msg="kdfhgkldfhgkl" />
 </template>
 
 <style scoped lang="scss">
